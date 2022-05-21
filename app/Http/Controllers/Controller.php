d8d9de6e9c7dc6e28446e3495a5f685ea232a0ff
l37sg0 <petar_1v@abv.bg>
2022-05-21 17:42:06 +0300

    [Basic website - Created]

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,11 +7,15 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+class Controller extends BaseController implements ContentInterface
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function index() {
-
+        $content = self::CONTENT;
+        $images  = scandir(public_path('images/gallery'));
+        unset($images[0]);
+        unset($images[1]);
+        return view('index', compact('content', 'images'));
     }
 }
